8cd5366c28aa284a42c8bab0f8bd69966221a30d
Anirudh <anisiva2005@gmail.com>
2025-10-21 22:56:19 +0530

    Step 3..

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -23,14 +24,33 @@ func main() {
 		go func(con net.Conn) {
 			defer con.Close()
 
+			buff := make([]byte, 108)
+			_, err := con.Read(buff)
+
+			if err != nil {
+				log.Fatal("Could not read from connection")
+			}
+			arr := strings.Split(string(buff), "\n")
+
+			reqLine := arr[0]
+
+			reqUrl := strings.Split(reqLine, " ")[1]
+
+			fmt.Println(reqUrl)
 			respStr := "HTTP/1.1 200 OK\r\n\r\n"
 			resp := []byte(respStr)
-			n, err := con.Write(resp)
-			if err != nil {
-				log.Fatal("Could not reply")
+
+			if reqUrl == "/" {
+				n, err := con.Write(resp)
+
+				if err != nil {
+					log.Fatal("Could not reply")
+				}
+				fmt.Printf("Wrote %v bytes\n", n)
+			} else {
+				con.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 			}
 
-			fmt.Printf("Wrote %v bytes\n", n)
 		}(con)
 	}
 }
